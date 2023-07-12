2e85d5f0479323540e453bb9e4c24e614a802850
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-07-12 16:44:39 +0200

    add data into screen Store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,5 +1,38 @@
 import './Store.css'
 
+const data = [
+  {
+      id:1,
+      etat: "des jumelles",
+      imgsrc: "/src/assets/Revenants.png",
+  },
+  {
+      id:2,
+      etat: "un lampe torche",
+      imgsrc: "/src/assets/Revenants.png",
+  },
+  {
+      id:3,
+      etat: "un briquet",
+      imgsrc: "/src/assets/Revenants.png",
+  },
+  {
+      id:4,
+      etat: "une montre",
+      imgsrc: "/src/assets/Revenants.png",
+  },
+  {
+      id:5,
+      etat: "un peigne",
+      imgsrc: "/src/assets/Revenants.png",
+  },
+  {
+      id:6,
+      etat: "un telephone",
+      imgsrc: "/src/assets/Revenants.png",
+  }
+];
+
 function Store() {
 
   return (
