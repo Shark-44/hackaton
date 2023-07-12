a7dfcf711770082405402e82721f38c69f016e86
Shark-44 <joanny.bernardeau@gmail.com>
2023-07-12 23:54:54 +0200

    composant de question et reponse pas des css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Prehistoire from './pages/Prehistoire'
+import RandomTableIndex from  './pages/RandomTableIndex'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +11,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -28,6 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Prehistoire/>
+      <RandomTableIndex />
     </>
   )
 }
