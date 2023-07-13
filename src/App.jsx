bcb9964a1086679201a37e575ac3682a2eea9c8b
Shark-44 <joanny.bernardeau@gmail.com>
2023-07-13 13:40:18 +0200

    avant un pull request

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Adventure from "./pages/Adventure";
 import Store from "./pages/Store";
 import Histoire from "/src/pages/Histoire"
+import Histoire2 from "/src/pages/Histoire2"
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/adventure" element={<Adventure />} />
         <Route path="/store" element={<Store />} />
         <Route path="/histoire" element={<Histoire />} />
+        <Route path="/Histoire2" element={<Histoire2 />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Histoire2.jsx
@@ -0,0 +1,34 @@
+import Prehistoire from '../components/Prehistoire';
+import RandomTableIndex from '../components/RandomTableIndex';
+import './Histoire.css'
+
+function Histoire2() {
+    return (
+        <div className='contenair'>
+            <img className="fondgrotte" src="src\images\pyramide.jpg" alt="fond" />
+            <div className='zoneperso'>
+            <div className='hCro'>
+                <div className='bulleHC'>
+                    <img className='tailleimg' src="src\images\bulle2.png" alt="bulle" />
+                <div className='zonereponse'>
+                    <RandomTableIndex />
+                </div>
+                </div>
+                <img src="src\images\egyptien.png" alt="homme cromagnon" />
+            </div>
+            <div className='jcquest'>
+                <div className='bulleJC'>
+                    <img className='imgbulle' src="src\images\bulle3.png" alt="bulle" />
+                    <div className='zonetext'>
+                        <Prehistoire />
+                    </div>
+                </div>
+                <img src="src\images\jcquest.png" alt="JC quest" />
+            </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Histoire2;
