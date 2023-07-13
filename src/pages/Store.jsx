15a217a4bfc52524b0257c723f9b0e34737c7b57
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-07-13 08:20:37 +0200

    add malette picture into store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -3,33 +3,39 @@ import './Store.css'
 const dataList = [
   {
       id:1,
-      etat: "des jumelles",
+      etat: "jumelles",
       imgsrc: "/src/assets/images/lunettes.jpg",
+      prix:"50$"
   },
   {
       id:2,
-      etat: "un lampe torche",
+      etat: "lampe torche",
       imgsrc: "/src/assets/images/flashlight.jpg",
+      prix: "30$"
   },
   {
       id:3,
-      etat: "un briquet",
+      etat: "briquet",
       imgsrc: "/src/assets/images/briquet.jpg",
+      prix: "10$"
   },
   {
       id:4,
-      etat: "une montre",
+      etat: "montre",
       imgsrc: "/src/assets/images/montre.jpg",
+      prix: "100$"
   },
   {
       id:5,
-      etat: "un peigne",
+      etat: "peigne",
       imgsrc: "/src/assets/images/peigne.jpg",
+      prix:"3$"
   },
   {
       id:6,
-      etat: "un telephone",
+      etat: "telephone",
       imgsrc: "/src/assets/images/smartphone.jpg",
+      prix: "1000$"
   }
 ];
 
@@ -39,7 +45,11 @@ function Store() {
     <>
       <div className="listStore">
         {dataList.map((data) => {
-          return <img src={data.imgsrc}/>
+          return <div className='maletteStore'>
+            <h2>{data.etat}</h2>
+            <img className='cardImg' src={data.imgsrc}/>
+            <p>{data.prix}</p>
+            </div>
         })}
       </div>
     </>
